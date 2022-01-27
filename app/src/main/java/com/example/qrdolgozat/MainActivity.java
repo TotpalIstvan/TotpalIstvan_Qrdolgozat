620a4593b75cc71f27d52d05f05616d599c17dd2
TotpalIstvan <totpal.istvan@petrik.hu>
2022-01-27 17:38:20 +0100

    fájlba írás + mentés

--- app/src/main/java/com/example/qrdolgozat/MainActivity.java
+++ app/src/main/java/com/example/qrdolgozat/MainActivity.java
@@ -1,16 +1,36 @@
 package com.example.qrdolgozat;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 
 public class MainActivity extends AppCompatActivity {
     private TextView qrTextView;
+    private boolean irE;
     private Button buttonScan, buttonKiir;
+    private static final String fajl = "scannedCodes.csv";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,11 +44,78 @@ public class MainActivity extends AppCompatActivity {
 
             intentIntegrator.initiateScan();
         });
+
+        buttonKiir.setOnClickListener(view -> {
+            if (irE) {
+                Date datum = Calendar.getInstance().getTime();
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                String szoveg = qrTextView.getText().toString() + ", " + dateFormat.format(datum);
+
+                try {
+                    FileOutputStream fileOutputStream = openFileOutput(fajl, MODE_PRIVATE);
+                    fileOutputStream.write(szoveg.getBytes());
+
+                    Toast.makeText(MainActivity.this, "Sikeresen elmentve: " + getFilesDir() + "/scannedCodes.csv", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Toast.makeText(MainActivity.this, "A fájlbaíráshoz engedélyezze a fájl hozzáférést!", Toast.LENGTH_SHORT).show();
+            }
+
+
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if (result != null) {
+            String sor = qrTextView.getText().toString();
+
+            qrTextView.setText((result.getContents()));
+            try {
+                Uri uri = Uri.parse(sor);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            } catch (Exception e) {
+                Log.d("URI ERROR", e.toString());
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
 
     public void init() {
             qrTextView = findViewById(R.id.qrTextView);
             buttonScan = findViewById(R.id.buttonScan);
             buttonKiir = findViewById(R.id.buttonKiir);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+            irE= false;
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        } else {
+            irE = true;
+        }
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 0) {
+            irE = ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                            == PackageManager.PERMISSION_GRANTED;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
+
